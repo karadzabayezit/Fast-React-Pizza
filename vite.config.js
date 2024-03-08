369e56f3eca072f63866308261fef70031ea2d71
karadza_b <bayezitkarajaew@gmail.com>
2024-03-08 17:07:14 +0500

    first

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import eslint from "vite-plugin-eslint";
+
+
+export default defineConfig({
+  plugins: [react(), eslint()],
+});
